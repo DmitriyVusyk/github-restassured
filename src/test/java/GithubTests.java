b14ae9986f8acc56db8e430c4cb8dd762a0d48c1
DmytroVusyk <kotmurkot4502@gmail.com>
2021-04-30 12:57:32 +0300

    add create/delete test

--- src/test/java/GithubTests.java
+++ src/test/java/GithubTests.java
@@ -7,8 +7,10 @@ import io.restassured.authentication.OAuthSignature;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -31,9 +33,66 @@ public class GithubTests {
     }
 
     @Test
+    @Order(0)
     public void getAllReposTest() {
         String userName = "dmitriyvusyk";
 
+        ArrayList<GithubRepoDTO> actualList = getAllRepositories(userName);
+        ArrayList<GithubRepoDTO> expectedList = new DataProvider().getTestData();
+
+        Assertions.assertTrue(Collections.disjoint(actualList, expectedList));
+    }
+
+    /*Второй тест будет создавать репу,
+    проверять что она появилась в списке репозиториев
+    и удалять,
+    после чего тоже проверять что она удалена*/
+
+    @Test
+    @Order(1)
+    public void createAndDeleteRepository() {
+        String userName = "dmitriyvusyk";
+        ArrayList<GithubRepoDTO> existingRepos = getAllRepositories(userName);
+        String expectedRepoName = "test_repository";
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("name", expectedRepoName);
+
+        Response createRepositoryResponse = given()
+                .auth().oauth2(GITHUB_PERSONAL_TOKEN, OAuthSignature.HEADER)
+                .body(requestParams.toString())
+                .when()
+                .post("/user/repos")
+                .then()
+                .log().ifError()
+                .statusCode(201)
+                .extract().response();
+
+        String body = createRepositoryResponse.getBody().asString();
+        Gson gson = new Gson();
+        GithubRepoDTO createdRepositoryDTO = gson.fromJson(body, GithubRepoDTO.class);
+
+        Assertions.assertEquals(createdRepositoryDTO.getName(), expectedRepoName, "created repository has expected name");
+
+        ArrayList<GithubRepoDTO> reposAfterPost = getAllRepositories(userName);
+
+        Assertions.assertTrue(reposAfterPost.size() > existingRepos.size(), "repository is created ");
+
+        given()
+                .auth().oauth2(GITHUB_PERSONAL_TOKEN, OAuthSignature.HEADER)
+                .pathParam("owner", userName)
+                .pathParam("repo", expectedRepoName)
+                .when()
+                .delete("/repos/{owner}/{repo}")
+                .then()
+                .log().ifError()
+                .statusCode(204);
+
+        ArrayList<GithubRepoDTO> reposAfterDelete = getAllRepositories(userName);
+        Assertions.assertTrue(Collections.disjoint(reposAfterDelete, existingRepos), "repository is deleted ");
+
+    }
+
+    private ArrayList<GithubRepoDTO> getAllRepositories(String userName) {
         Response response = given()
                 .auth().oauth2(GITHUB_PERSONAL_TOKEN, OAuthSignature.HEADER)
                 .basePath(USERS_ENDPOINT)
@@ -48,11 +107,8 @@ public class GithubTests {
         String body = response.getBody().asString();
         Gson gson = new Gson();
 
-        ArrayList<GithubRepoDTO> actualList = gson.fromJson(body, new TypeToken<ArrayList<GithubRepoDTO>>() {
+        return gson.fromJson(body, new TypeToken<ArrayList<GithubRepoDTO>>() {
         }.getType());
-        ArrayList<GithubRepoDTO> expectedList = new DataProvider().getTestData();
-
-        Assertions.assertTrue(Collections.disjoint(actualList, expectedList));
     }
 
 }
